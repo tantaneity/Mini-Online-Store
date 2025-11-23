35fe84c72615bc7e1f9d634e3233fb2186c4a58b
tantaneity <157833402+tantaneity@users.noreply.github.com>
2025-11-23 07:05:24 +0200

    fix: standardize quotes and formatting in Storybook configuration

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,20 +1,23 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@chromatic-com/storybook",
     "@storybook/addon-docs",
     "@storybook/addon-onboarding",
     "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
+    "@storybook/addon-vitest",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    if (config.mode === "production") {
+      config.base = "/mini-store/storybook/";
+    }
+    return config;
+  },
 };
 export default config;
